82a01d7017da6fdde44b4a75ed42a5c5f4370f13
John Gargalionis <johngargalionis@gmail.com>
2022-06-24 00:06:37 +0200

    Factored out Parse.m

--- Parse.m
+++
@@ -1,9 +0,0 @@
-<<MatchMakerParser`;
-
-{fileName, name, matchMakerDataPath, outputPythonFilePath, configFilePath} = $ScriptCommandLine;
-
-(* Evaluate config file *)
-Get[configFilePath];
-
-(* Output Python file *)
-MatchMakerToPython[name, matchMakerDataPath, outputPythonFilePath];
