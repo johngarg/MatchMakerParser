437c9ae714a260735d17dfd515bf3fa7d5e1dc42
John Gargalionis <johngargalionis@gmail.com>
2024-01-24 17:38:43 +0100

    Fixed bug in Yukawas

--- python/matchingresult.py
+++ python/matchingresult.py
@@ -11,23 +11,25 @@ MZ = 91.1876 * GEV
 
 class GenericMatchingResult:
     """Class for storing MatchMaker matching results. All dimensionful quantities
-    are in TeV. Scale is initialised to mZ.
+    are in TeV. Scale is initialised to 1 TeV.
 
     """
 
-    def __init__(self, name: str, scale: float = MZ):
+    def __init__(self, name: str, scale: float = 1.0):
         self.name = name
         self.scale = scale
 
         # See https://arxiv.org/abs/2112.10787 for more information about
         # specific couplings and constants
 
-        self.vev: float = 246 * GEV
+        vev_gev: float = 246
+        self.vev: float = vev_gev * GEV
+
         # SM couplings and parameters
         # Values taken from https://inspirehep.net/files/71930f37457ebd10742e55c2b36531ea
-        self.yu: np.matrix = np.diag([0.00123, 0.620, 168.26]) / self.vev
-        self.yd: np.matrix = np.diag([0.00267, 0.05316, 2.839]) / self.vev
-        self.yl: np.matrix = np.diag([0.00048307, 0.101766, 1.72856]) / self.vev
+        self.yu: np.matrix = np.diag([0.00123, 0.620, 168.26]) / vev_gev
+        self.yd: np.matrix = np.diag([0.00267, 0.05316, 2.839]) / vev_gev
+        self.yl: np.matrix = np.diag([0.00048307, 0.101766, 1.72856]) / vev_gev
 
         self.yubar: np.matrix = self.yu
         self.ydbar: np.matrix = self.yd
@@ -40,17 +42,25 @@ class GenericMatchingResult:
         self.g2: float = 0.357254
         self.g3: float = 1.204
 
-        self.muH: float = np.sqrt(self.mH**2 / 2)
-        self.lam: float = self.mH**2 / (2 * self.vev**2)
-
         # Constants
         self.iCPV: int = 1  # Fixes \eps_{0123} sign convention
         self.onelooporder: int = 1  # Dummy variable to identify 1-loop part
         self.aEV: float = 1  # Evanescent parameter
-        self.mu: float = self.scale  # Identify mu with scale
         self.invepsilonbar: float = 0
         self.epsilonbar: float = 0
         self.xRP: float = 1 # Reading-point parameter
+
+    @property
+    def muH(self):
+        return np.sqrt(self.mH**2 / 2)
+
+    @property
+    def lam(self):
+        return self.mH**2 / (2 * self.vev**2)
+
+    @property
+    def mu(self):
+        return self.scale
 
     def kronecker_delta(self, i, j):
         if i == j:
